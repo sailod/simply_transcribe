4e0b8234cc6c6ac6c9cc87ee9bf46c219e76024a
Roi <roezairi@gmail.com>
2025-11-16 04:03:13 +0000

    Add LOCAL_MODEL environment variable for local Whisper model in transcribe-hotkey.sh

--- electron-transcribe/transcribe-hotkey.sh
+++ electron-transcribe/transcribe-hotkey.sh
@@ -2,6 +2,7 @@
 # this is script to allow injection of the right needed env variables for the hotkey trigger
 # youll need to run those after extracting the AppImage (./electron-transcribe-1.0.0.AppImage --appimage-extract)
 # sudo chown root:root chrome-sandbox && sudo chmod 4755 chrome-sandbox
+export LOCAL_MODEL="Xenova/whisper-tiny.en"
 export USE_OPENAI_API=false
 export OPENAI_API_TOKEN="REPLACE_WITH_YOUR_OPENAI_API_TOKEN"
 /home/roi/apps/simply_transcribe/squashfs-root/AppRun
